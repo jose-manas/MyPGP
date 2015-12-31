fc22abbae382e158b8894774830cee83e7b32d87
jmanas <jmanas@dit.upm.es>
2015-12-31 06:31:05 +0100

    bouncy castle libraries manually download onto HOME/lib.skip

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,11 +1,13 @@
 package gui;
 
+import bc.ClasspathHacker;
 import exception.MyLogger;
 import gui.imgs.Icons;
 import keys.Info;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.prefs.Preferences;
 
 /**
@@ -18,6 +20,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+//            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
             Preferences preferences = Preferences.userRoot().node("mypgp");
 
             String langPreference = preferences.get("language", "en");
@@ -44,6 +48,19 @@ public class Main {
                 throw new NoHomeException();
             preferences.put("root", home.getCanonicalPath());
             Info.setHome(home);
+
+            File jarlib = new File(home, "lib.skip");
+            if (!(jarlib.exists() && jarlib.isDirectory()))
+                jarlib = home;
+
+            File[] jars = jarlib.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".jar");
+                }
+            });
+
+            for (File file : jars)
+                ClasspathHacker.addFile(file);
 
             MyPGP.start();
         } catch (NoHomeException nhe) {
