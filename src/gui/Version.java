e6bc9f7405daf193b047bfe9cb729cd5546722d7
josé a. mañas <jose.manas@gmail.com>
2021-06-21 17:35:52 +0200

    recompiled with BouncyCastle 1.69

--- src/gui/Version.java
+++ src/gui/Version.java
@@ -4,5 +4,5 @@ package gui;
  * @author Jose A. Manas
  */
 public class Version {
-    public static final String VERSION = "MyPGP (2.4.2021)";
+    public static final String VERSION = "MyPGP (21.6.2021)";
 }
