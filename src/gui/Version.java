23d5d1e696ca7db7ab20ad0b84e604252a7e2a6e
josé a. mañas <jose.manas@gmail.com>
2021-11-19 17:11:36 +0100

    update version

--- src/gui/Version.java
+++ src/gui/Version.java
@@ -4,5 +4,5 @@ package gui;
  * @author Jose A. Manas
  */
 public class Version {
-    public static final String VERSION = "MyPGP (21.6.2021)";
+    public static final String VERSION = "MyPGP (19.11.2021)";
 }
